4b25f2928e484d168a4e1b21bb8b234a77796829
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2016-03-13 10:53:34 +0100

    Creado TrainingDao

---
+++ src/main/java/data/daos/TrainingDao.java
@@ -0,0 +1,13 @@
+package data.daos;
+
+import java.util.Calendar;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import data.entities.Training;
+
+public interface TrainingDao extends JpaRepository<Training, Integer> {
+
+    List<Training> findTrainingsByStartDate(Calendar startDate);
+}
